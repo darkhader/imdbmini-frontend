9b52ce1f3f5572efc284a3ca57571b83cb91a4ed
Nguyễn Hoàng Hiệp <40733446+darkhader@users.noreply.github.com>
2018-12-09 00:07:57 +0700

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export default {
   rootPath:
-    process.env.REACT_APP_STATE === "localhost" ? "https://imdb-mini.herokuapp.com/" : ""
+    process.env.REACT_APP_STATE === "localhost" ? "https://imdb-mini.herokuapp.com" : ""
 };
