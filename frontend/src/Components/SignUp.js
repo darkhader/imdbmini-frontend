cae83e0ab6f0e7390e5278734e41b25af2c3a655
Hiep <dr.hiep98@gmail.com>
2018-12-16 16:09:45 +0700

    OOHHHHH

--- frontend/src/Components/SignUp.js
+++ frontend/src/Components/SignUp.js
@@ -29,7 +29,7 @@ class SignUp extends Component {
             .then(response => {
                 console.log(response.data);
                 if (response.data.success) {
-                    window.location.href = "/";
+                    window.location.href = this.props.match.params;
                 }
             })
             .catch(err => console.log(err))
